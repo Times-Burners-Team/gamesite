c46102479caec344573a8ee3e5c00ff645132909
Dmitrii <timesburnersteam@gmail.com>
2017-12-27 16:17:59 -0800

    Update

--- mainPage/urls.py
+++ mainPage/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from . import views
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),

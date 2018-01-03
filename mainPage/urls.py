ebed0f040702dcdd59553ffa5f3f540e27bee9b8
Dmitrii <timesburnersteam@gmail.com>
2018-01-03 00:21:43 -0800

    1

--- mainPage/urls.py
+++ mainPage/urls.py
@@ -3,10 +3,8 @@ from django.conf.urls import url, include
 from . import views
 from django.conf.urls.static import static
 from newsPage.models import Articles
-from django.views.generic import ListView, DetailView
 
 urlpatterns = [
-    url(r'^$', ListView.as_view(queryset=Articles.objects.all().order_by("-date")[:20], template_name="news/posts.html")),
-    url(r'^(?P<pk>\d+)$', DetailView.as_view(model = Articles, template_name = "news/post.html")),
     url(r'^$', views.index, name='index'),
+    
 ]
--- mainPage/views.py
+++ mainPage/views.py
@@ -1,4 +1,23 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from newsPage.models import Articles
 
 def index(request):
     return render(request, 'mainPage/homePage.html')
+
+def news(request):
+    posts_list = Post.objects.all().order_by('-timestamp')
+    paginator = Paginator(posts_list, 8)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    vars = dict(
+        posts=posts,
+        )
+    return render_to_response('news/posts.html', vars, context_instance=RequestContext(request))
